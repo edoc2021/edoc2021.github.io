3ae5f2690eb7dbcf5eee944eb8664530d3eb46bd
vu-luong <vubinhcht@gmail.com>
2020-11-16 12:19:07 +1000

    :zap: fix: change to domain /2021

--- src/components/navibar/navibar.js
+++ src/components/navibar/navibar.js
@@ -20,25 +20,25 @@ const Navibar = ({ sticky, location, title }) => {
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
                 <HoverDropdown href="" title="Program" id="collasible-nav-dropdown-1">
-                  <NavDropdown.Item as={Link} to="/keynote/">Keynotes</NavDropdown.Item>
-                  {/*<NavDropdown.Item href="/accepted-papers">Accepted papers</NavDropdown.Item>*/}
+                  <NavDropdown.Item as={Link} to="/2021/keynote">Keynotes</NavDropdown.Item>
+                  {/*<NavDropdown.Item as={Link} to="/2021/accepted-papers">Accepted papers</NavDropdown.Item>*/}
                 </HoverDropdown>
                 <HoverDropdown href="" title="Conference" id="collasible-nav-dropdown-2">
-                  <NavDropdown.Item as={Link} to="/call-papers/">Call for Papers</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/committees/">Committees</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/important-dates">Important Dates</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/2021/call-papers/">Call for Papers</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/2021/committees/">Committees</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/2021/important-dates">Important Dates</NavDropdown.Item>
                   <NavDropdown.Item href="http://www.ieee-edoc.org/">Previous EDOCs</NavDropdown.Item>
                 </HoverDropdown>
                 <HoverDropdown title="Workshops, Demos, Doctoral Consortium" id="collasible-nav-dropdown-3">
-                  <NavDropdown.Item as={Link} to="/workshops">Call for Workshops</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/call-demos">Call for Demos</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/doctoral-consortium">Doctoral Consortium</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/2021/workshops">Call for Workshops</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/2021/call-demos">Call for Demos</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/2021/doctoral-consortium">Doctoral Consortium</NavDropdown.Item>
                   {/*<NavDropdown.Divider />*/}
                   {/*<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>*/}
                 </HoverDropdown>
-                {/*<Nav.Link as={Link} to="/registration">Registration</Nav.Link>*/}
-                {/*<Link to="/program">Registration</Link>*/}
-                <Nav.Link href="/venue">Venue</Nav.Link>
+                {/*<Nav.Link as={Link} to="/2021/registration">Registration</Nav.Link>*/}
+                {/*<Link to="/2021/program">Registration</Link>*/}
+                <Nav.Link as={Link} to="/2021/venue">Venue</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,14 @@
 import React from "react"
-import { graphql } from "gatsby"
-
-import Layout from "../components/layout/layout"
+import { Container } from "react-bootstrap"
 
 class IndexPage extends React.Component {
   render() {
-    const { posts } = this.props.data
-
     return (
-      <Layout location={this.props.location}>
-        <div className="article">
-          <div className="container">
-            <h1 style={{ color: "#2c4f90" }}>EDOC2021</h1>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: posts.edges[0].node.html || ""
-              }}
-            />
-          </div>
-        </div>
-      </Layout>
+      <Container>
+        <hr />
+        <h1>Welcome to EDOC conference!</h1>
+        <hr />
+      </Container>
     )
   }
 }
@@ -27,17 +16,3 @@ class IndexPage extends React.Component {
 
 export default IndexPage
 
-export const query = graphql`
-  query {
-    posts: allMarkdownRemark (
-      sort: { fields: [frontmatter___date], order: DESC },
-      filter: {frontmatter: {type: {eq: "post"}}}
-    ) {
-      edges {
-        node {
-          html
-        }
-      }
-    }
-  }
-`
