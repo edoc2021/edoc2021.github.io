ef6e80f8cfd0b0f0ea889c8e927da2c8c9290e84
vu-luong <vubinhcht@gmail.com>
2020-11-17 16:31:18 +1000

    :zap: fix: update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { navigate } from "gatsby"
 class IndexPage extends React.Component {
 
   componentDidMount() {
-    navigate("/EDOC2021");
+    navigate("/EDOC2021SUB");
   }
 
   render() {
