ccf50360e76b884d986fa2d6dac6ee4865ac13ac
vu-luong <vubinhcht@gmail.com>
2020-11-17 16:46:12 +1000

    :zap: fix: 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,13 @@
 import React from "react"
-import Layout from "../components/layout/layout"
 import { Container } from "react-bootstrap"
 
 
 const NotFoundPage = () => (
-  <Layout>
-    <Container>
-      <h1>404: Not Found</h1>
-      <hr/>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Container>
-  </Layout>
+  <Container>
+    <h1>404: Not Found</h1>
+    <hr />
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Container>
 )
 
 export default NotFoundPage
